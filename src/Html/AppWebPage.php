d0f162782d7dea4819ee6f866f01a793a72eaf40
Adrien Cadalen <adrien.cadalen@etudiant.univ-reims.fr>
2024-06-10 14:36:20 +0200

    Ajout de la classe AppWebPage

---
+++ src/Html/AppWebPage.php
@@ -0,0 +1,22 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Html;
+
+class AppWebPage extends WebPage
+{
+    use StringEscaper;
+    public function __construct(string $title = "")
+    {
+        parent::__construct($title);
+        $this->appendCssUrl('css/style.css');
+    }
+
+    public function toHTML(): string
+    {
+        $htmlHead = "<head>{$this->getHead()} \n<meta charset=\"UTF-8\"> \n<meta name=\"viewport\" content=\"width=device-width, initial-scale=1\" /> \n<title>{$this->getTitle()}</title>\n</head>\n";
+        $htmlBody = "<body> <div class='header'><h1>{$this->getTitle()}</h1></div> <div class='content'>\n{$this->getBody()}</div> <div class='footer'> Dernière modification : {$this->getLastModification()}</div> </body>";
+        return "<!doctype HTML><html lang=\"fr\"> " . $htmlHead . $htmlBody . "</html>";
+    }
+}
