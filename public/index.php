9c57ccdf3ac376f81c9262f4466453955e57592f
souf0012 <bounany-toustary.souf-daoud@etudiant.univ-reims.fr>
2024-06-10 14:00:41 +0200

    Ajout du debut de la création de la page

--- public/index.php
+++ public/index.php
@@ -1,4 +1,13 @@
 <?php
+declare(strict_types=1);
 
+use html\WebPage ;
+use Entity\Episode;
+use Entity\Poster ;
+use Entity\Show ;
 
 echo "Hello bienvenue sur notre site";
+
+$Page = New WebPage();
+$Page->setTitle("Séries TV");
+$Page->appendContent("<div> class='menu' <a href ='src/Entity/Episode.php?name={$name->getName()}'>Titre Serie</a></div>");
