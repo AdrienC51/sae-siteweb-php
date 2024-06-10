21c966371ea712b8aa05b17bc88fc59729a33135
Adrien Cadalen <adrien.cadalen@etudiant.univ-reims.fr>
2024-06-10 10:50:54 +0200

    Ajout de la classe ShowCollection

---
+++ src/Entity/Collection/ShowCollection.php
@@ -0,0 +1,25 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Entity\Collection;
+
+use Entity\Show;
+use Database\MyPdo;
+use PDO;
+
+class ShowCollection
+{
+    public function findAll():array
+    {
+        $allShow = MyPDO::getInstance()->prepare(
+            <<<SQL
+                SELECT id, name, originalName, overview
+                FROM tvshow
+                ORDER BY name
+            SQL
+        );
+        $allShow->execute();
+        return $allShow->fetchAll(PDO::FETCH_CLASS, 'Entity\Show');
+    }
+}
