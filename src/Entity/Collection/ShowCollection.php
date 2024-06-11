4f449e4a5b714a18a7811936637b09fcf87b2908
Adrien Cadalen <adrien.cadalen@etudiant.univ-reims.fr>
2024-06-11 15:53:56 +0200

    Ajout de prise en compte d'une donnée genreId, par défaut null

--- src/Entity/Collection/ShowCollection.php
+++ src/Entity/Collection/ShowCollection.php
@@ -10,15 +10,27 @@ use PDO;
 
 class ShowCollection
 {
-    public function findAll(): array
+    public function findAll(int $genreId = null): array
     {
-        $allShow = MyPDO::getInstance()->prepare(
-            <<<SQL
-                SELECT id, name, originalName, overview, posterId
-                FROM tvshow
-                ORDER BY name
-            SQL
-        );
+        if (empty($genreId)) {
+            $allShow = MyPDO::getInstance()->prepare(
+                <<<SQL
+                    SELECT id, name, originalName, overview, posterId
+                    FROM tvshow
+                    ORDER BY name
+                SQL
+            );
+        } else {
+            $allShow = MyPDO::getInstance()->prepare(
+                <<<SQL
+                    SELECT DISTINCT t.id, name, originalName, overview, posterId
+                    FROM tvshow t
+                    JOIN tvshow_genre tvg on (t.id = tvg.tvShowId) 
+                    WHERE tvg.genreId = {$genreId}
+                    ORDER BY name
+                SQL
+            );
+        }
         $allShow->execute();
         return $allShow->fetchAll(PDO::FETCH_CLASS, 'Entity\Show');
     }
