c5c6fcbd78f256de83aa7f21590d305caa4ae752
Adrien Cadalen <adrien.cadalen@etudiant.univ-reims.fr>
2024-06-11 14:09:36 +0200

    Ajout de la méthode getHtmlForm pour la classe ShowForm

--- src/Html/Form/ShowForm.php
+++ src/Html/Form/ShowForm.php
@@ -5,9 +5,12 @@ declare(strict_types=1);
 namespace Html\Form;
 
 use Entity\Show;
+use Html\StringEscaper;
 
 class ShowForm extends Show
 {
+    use StringEscaper;
+
     public ?Show $show;
 
     public function __construct(?Show $show = null)
@@ -17,5 +20,33 @@ class ShowForm extends Show
     public function getShow(): ?Show
     {
         return $this->show;
+    }
+
+    public function getHtmlForm(string $action): string
+    {
+        return <<<HTML
+                    <!DOCTYPE html>
+                    <form method="post" action="{$action}">
+                        <input name="id" type="hidden" value="{$this?->show?->getId()}">
+                        <label>
+                            Nom français
+                            <input name="name" type="text" value="{$this->escapeString($this?->show?->getName())}" required>
+                        </label>
+                        <label>
+                            Nom original
+                            <input name="originalName" type="text" value="{$this->escapeString($this?->show?->getOriginalName())}" required>
+                        </label>
+                        <label>
+                            Page d'accueil
+                            <input name="homepage" type="text" value="{$this->escapeString($this?->show?->getHomepage())}" required>
+                        </label>
+                        <label>
+                            Description
+                            <input name="overview" type="text" value="{$this->escapeString($this?->show?->getOverview())}" required>
+                        </label>
+                        
+                        <button type="submit">Enregistrer</button>
+                    </form>
+                    HTML;
     }
 }
